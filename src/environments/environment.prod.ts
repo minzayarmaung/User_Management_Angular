938da2c0160b9c4902b57f9167495020bb571467
Min Zayar Maung <minzayarmaung2002@gmail.com>
2026-02-08 01:07:51 +0630

    ADDED

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://user-management-backend-xhk9.onrender.com/api/v1'
+    apiUrl: 'https://user-management-backend-xhk9.onrender.com/user-management/api/v1'
 };
